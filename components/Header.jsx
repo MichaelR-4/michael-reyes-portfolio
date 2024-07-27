f41fb52702efdd096790a57a6b894fd1a85842bc
Michael Reyes <Michaelreyes499@gmail.com>
2024-07-26 21:25:46 -0400

    created desktop navbar and mobile navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,18 +1,31 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 
-const Header = (props) => {
+// components
+import Nav from "./Nav";
+
+const Header = () => {
   return (
     <>
       <header className="py-8 xl:py-12 text-white bg-pink-50/20">
-        <div className="container mx-auto">
+        <div className="container mx-auto flex justify-between items-center">
           {/* logo */}
           <Link href="/">
             <h1 className="text-4xl font-semibold">
               Michael Reyes<span className="text-accent">.</span>
             </h1>
           </Link>
-          {/* desktop nav */}
+
+          {/* desktop nav and hire me button */}
+          <div className="hidden xl:flex items-center gap-8">
+            <Nav />
+            <Link href="/contact">
+              <Button>Hire Me</Button>
+            </Link>
+          </div>
+
+          {/* mobile nav */}
+          <div className="xl:hidden">mobile nav</div>
         </div>
       </header>
     </>
