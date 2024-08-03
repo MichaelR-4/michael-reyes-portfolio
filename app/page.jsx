36cd3740c8e9f93a51866f0cbe6dcca303821679
Michael Reyes <Michaelreyes499@gmail.com>
2024-08-02 22:36:19 -0400

    added socials to hero section

--- app/page.jsx
+++ app/page.jsx
@@ -29,8 +29,12 @@ const Home = () => {
                 <span>Latest Projects</span>
                 <GoArrowDown className="text-xl" />
               </Button>
-              <div className="mb-8 xl:mb-0">socials</div>
-              <Socials />
+              <div className="mb-8 xl:mb-0">
+                <Socials
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transparent-all duration-500"
+                />
+              </div>
             </div>
           </div>
           <div>Photo</div>
